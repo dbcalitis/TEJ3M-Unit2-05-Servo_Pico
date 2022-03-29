6622309963bf27e7db600aa74b18bc2272f25a44
Daria Calitis <60710963+dbcalitis@users.noreply.github.com>
2022-03-29 11:20:20 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,23 +1,8 @@
-# It rotates the servo back and forth.
-
-# Created by: Daria Bernice Calitis
-# Created on: Feb 2022
-
 import time
 import board
-import pwmio
-from adafruit_motor import servo
-
-# create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.GP27, duty_cycle=2 ** 15, frequency=50)
-
-# Create a servo object, my_servo.
-my_servo = servo.Servo(pwm)
+import adafruit_hcsr04
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP13, echo_pin=board.GP14)
 
 while True:
-    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-        my_servo.angle = angle
-        time.sleep(0.05)
-    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
-        my_servo.angle = angle
-        time.sleep(0.05)
+    print((sonar.distance,))
+    time.sleep(0.1)
